3a4748a1ad4031b09d14190227f29d8d4a9ac2ec
lhx980327 <62562058+lhx980327@users.noreply.github.com>
2020-04-26 19:28:54 -0700

    Update AIinput.py

--- AIinput.py
+++ AIinput.py
@@ -1,16 +1,6 @@
+#!/usr/bin/env python
+#coding:utf-8
 
-
-static int Keyboard_input() {
-    char c;
-    while(1) {
-        //c = getchar();
-        c = getch();
-        switch(c) {
-            case 'w': return UP;
-            case 'a': return LEFT;
-            case 's': return DOWN;
-            case 'd': return RIGHT;
-            case 'q': return QUIT;
-        }
-    }
-}
+class AIinput:
+	def getMove(self, grid):
+		pass
